4f511e44be134318d6f7f69a9216e41d71cfd65f
Danilo Spinelli <danilo.spinelli@pagopa.it>
2026-03-10 13:56:35 +0000

    renaming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Engineering Radar",
+  title: "Engineering Metrics",
   description: "DX Engineering Metrics Dashboard",
 };
 
